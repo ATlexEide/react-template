23696a7d1fe3e6bc53be5ee539c0f04b23c3aef1
Alex <atlexeide@gmail.com>
2025-05-17 18:36:08 +0200

    Add header and footer call

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Header from "./components/main/Header.jsx";
+import Footer from "./components/main/Footer.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header />
     <App />
-  </StrictMode>,
-)
+    <Footer />
+  </StrictMode>
+);
